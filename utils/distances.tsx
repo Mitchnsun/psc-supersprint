b4dbdb91ac13bcb0551a98e803ec768c5941aca9
Matthieu Compérat <matthieu.comperat@gmail.com>
2024-01-14 12:53:27 +0100

    feat(distances): Distances 2024

--- utils/distances.tsx
+++ utils/distances.tsx
@@ -5,6 +5,25 @@ export default {
     unit_label: 'mètres',
   },
   bike: {
+    length: 6,
+    unit: 'km',
+    unit_label: 'kilomètres',
+  },
+  run: {
+    length: 2.4,
+    unit: 'km',
+    unit_label: 'kilomètres',
+  },
+};
+
+/* 2023
+export default {
+  swim: {
+    length: 300,
+    unit: 'm',
+    unit_label: 'mètres',
+  },
+  bike: {
     length: 5.8,
     unit: 'km',
     unit_label: 'kilomètres',
@@ -15,6 +34,7 @@ export default {
     unit_label: 'kilomètres',
   },
 };
+*/
 
 /* 2022
 export default {
