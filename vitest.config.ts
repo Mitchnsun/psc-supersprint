6e79280cf8800fe2ae21bf58d0355b14ef64e80a
Matthieu Compérat <matthieu.comperat@gmail.com>
2024-03-16 13:22:17 +0100

    tech(test): add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true, // allows us to use vitest library methods in unit test without explicit imports
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+    coverage: {
+      include: ['**/*.{ts,tsx}'], // specify files to include
+      exclude: ['storybook-static/**', '**/*.stories.tsx'], // specify files to exclude
+      reporter: ['text', 'html'], // customize reporters. don't forget to include 'html' if you use vitest-ui
+    },
+  },
+});
